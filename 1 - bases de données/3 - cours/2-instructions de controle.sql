4fecf1cc7e76e1a8029dd6cabbae8f2a0299de84
rahmouni oussama <ousrah@hotmail.com>
2026-09-23 09:32:51 +0000

    case

--- 1 - bases de données/3 - cours/2-instructions de controle.sql
+++ 1 - bases de données/3 - cours/2-instructions de controle.sql
@@ -245,6 +245,7 @@ select premier (2,3);
 
 #exercice 2
 #ecrire un algorithme qui permet de resoudre une euqation de deuxième degrès
+/*
 Ax²+Bx+C = 0 
 A=0, B=0, C= 0  x = R
 A=0, B=0,C<>0 x=impossible
@@ -258,6 +259,45 @@ A<>0
     
 select pow(5,3);    
 select sqrt(25);
+*/
+drop function if exists deuxiemme;
+delimiter $$
+create function if not exists deuxiemme(a float, b float,c float)
+	returns varchar(150)
+    deterministic
+begin 
+	declare delta float;
+	if a=0 then
+		if b=0 then
+			if c=0 then
+				return 'x est l''ensemble R';
+			else 
+				return 'impossible';
+			end if;
+		else
+			return concat("x=",-c/b);
+        end if;
+	else
+		set delta= (b*b) - (4*a*c);
+        if delta>0 then
+			return concat( "x1=",(-b-sqrt(delta))/(2*a), " x2=",(-b+sqrt(delta))/(2*a));
+		elseif delta=0 then
+			return concat("x1=x2=",-b/(2*a));
+		else
+			return 'impossible dans R';
+		end if;
+	end if;
+end $$
+delimiter ;
+
+
+select deuxiemme(0,0,0); 
+select deuxiemme(0,0,3); 
+select deuxiemme(0,3,2); 
+select deuxiemme(4,4,1);  # delta = 0
+select deuxiemme(4,6,1);  # delta > 0
+select deuxiemme(4,1,1);  # delta < 0
+
 
 
 #exercice 3
@@ -267,7 +307,130 @@ select sqrt(25);
 #si il est marié il aura 25% au lieu de 20%
 # pour chaque enfant il va avoir 10% avec un plafond de 50%
 # si il a un salaire inférieur à 3000 dh il aura un surplus de 10%
+drop function if exists participation;
+delimiter $$
+create function participation(prix decimal, marie boolean , nb_enfant int , salair decimal)
+	returns varchar(150)
+    deterministic
+begin 
+	declare pourcentage int;
+    set pourcentage = 20;
+    if marie = true then 
+		set pourcentage=pourcentage+5;
+    end if;
+    set pourcentage=pourcentage+(nb_enfant*10);
+    if pourcentage>50 then 
+		set pourcentage=50;
+	end if;
+    if salair<3000 then
+		set pourcentage=pourcentage+10;
+    end if;
+    return concat("le montant payer par le patron est ",round((pourcentage*prix)/100 ,2));
+end $$
+delimiter ;
+
+select participation(100,false,0,8000);
+select participation(100,false,0,2000);
+select participation(100,true,7,2000);
+select participation(100,true,2,7000);
 
 
+# ecrire une fonction qui reçoit le numero du jour et qui retourn son nom en français
+#exemple select nom_jour(1)  -->  'dimanche';
+#			   nom_jour(6)  -->  'venderid';
+               
+ drop function if exists num_jour;
+delimiter $$
+create function num_jour(nb int)
+	returns varchar(150)
+    deterministic
+begin
+	if nb=1 then 
+		return 'dimanche';
+	elseif nb=2 then 
+		return 'lundi' ;
+	elseif nb=3 then 
+		return 'mardi' ;
+	elseif nb=4 then 
+		return 'mercredi' ;
+	elseif nb=5 then 
+		return 'jeudi' ;
+	elseif nb=6 then 
+		return 'vendredi' ;
+	elseif nb=7 then 
+		return 'samedi' ;
+	else 
+		return 'numero invalide';
+	end if;
+end $$
+delimiter ;
+select num_jour(1);
+ 
+ 
+ 
+            
+ drop function if exists num_jour;
+delimiter $$
+create function num_jour(nb int)
+	returns varchar(150)
+    deterministic
+begin
+	declare jour varchar(150);
+    case 
+		when nb=1 then set jour = "Dimanche";
+		when nb=2 then set jour = "Lundi";
+		when nb=3 then set jour = "Mardi";
+		when nb=4 then set jour = "Mercredi";
+		when nb=5 then set jour = "Jeudi";
+		when nb=6 then set jour = "Vendredi";
+		when nb=7 then set jour = "samedi";
+    else
+		set jour = "incorrect";
+	end case;
+    return jour;
+end $$
+delimiter ;
+
+
+
+ drop function if exists num_jour;
+delimiter $$
+create function num_jour(nb int)
+	returns varchar(150)
+    deterministic
+begin
+	declare jour varchar(150);
+    set jour = case nb
+					when 1 then  "Dimanche"
+					when 2 then  "Lundi"
+					when 3 then  "Mardi"
+					when 4 then "Mercredi"
+					when 5 then  "Jeudi"
+					when 6 then  "Vendredi"
+					when 7 then  "samedi"
+				else
+					 "incorrect"
+				end;
+    return jour;
+end $$
+delimiter ;
+
+
+select num_jour(2);
+
+
+
+
+# exercice : ecrire une fonction qui récupère une note et qui affiche sa mention
+# en respectant les valeurs suivantes
+# si note < 5 très faible
+# si note entre 5 et <9 faible
+# si note entre 9 et <10 insuffisant
+# si note entre 10 et <12 passable
+# si note entre 12 et <14 assez bien
+# si note entre 14 et <16 bien
+# si note entre 16 et <18 très bien
+# si note entre 18 et <=20 excellent
+# si note non inclus entre 0 et 20 erreur
 
 
